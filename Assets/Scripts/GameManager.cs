91cc1c8ce4166800075713578d027c56df10cfa3
Ruchir Bapat <ruchir.bapat@gmail.com>
2019-05-20 18:42:10 +1000

    starting ui, fixed lighting

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    int currentLevel;
+    float enemiesAlive;
+
+    void Awake()
+    {
+        currentLevel = 1;
+    }
+
+    void Update()
+    {
+        enemiesAlive = GameObject.FindObjectsOfType<Enemy>().Length;
+        if(enemiesAlive <= 0)
+        {
+            LoadLevel(++currentLevel);
+        }
+    }
+
+    void LoadLevel(int level)
+    {
+        SceneManager.LoadScene("Level " + level);
+        Vector3 playerScreenPos = Camera.main.WorldToScreenPoint(FindObjectOfType<Player>().transform.position);
+    }
+
+}
