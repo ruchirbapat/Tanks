56747f8f007e4cd5fb5f6837e162f90543a13ee2
Ruchir Bapat <ruchir.bapat@gmail.com>
2019-06-04 21:24:25 +1000

    Finished commenting

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,49 +1,71 @@
-﻿using UnityEngine;
+﻿// Dependencies
+using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
 
-    public LayerMask collidable;
-    public static int bounceable;
-    public Color trailColour;
-    public float trailDuration;
-    public float speed = 10;
-    public float lifetime = 3f;
-    public float gunDamageAmount;
-    public int maxBounces;
-    private int bounces = 0;
-    private CameraShake camShake;
+    // Public properties
+    public LayerMask collidable; // LayerMask indicates which surfaces the Bullet may collide with
+    public static int bounceable; // Stores the layer mask of the bounceable surface
+    public Color trailColour; // Property assigned to the colour of the trail (set in Unity Editor)
+    public float trailDuration; // How long the trail should be cast behind the Bullet
+    public float speed = 10; // Speed of the Bullet, which is updated by the gun
+    public float lifetime = 3f; // How long the bullet lasts before automatically being destroyed
+    public float gunDamageAmount; // How much damage the Bullet deals
+    public int maxBounces; // Maximum number of bounces the Bullet can have before being destroyed
+    private int bounces = 0; // How many bounces have occurred
+    private CameraShake camShake; // Refernce to CameraShaker GameObject
 
+    // Audio source reference so SFX can be played
     AudioSource audioSource;
 
     private void Awake()
     {
+        // Set the layer of which the bullet can bounce against i.e. find the "environment" objects' layer
         bounceable = GameObject.FindGameObjectsWithTag("Environment")[0].gameObject.layer;
     }
 
     void OnDestroy() {
-        try { Camera.main.GetComponent<CameraShake>().Shake(); } catch { };
+        // Shake the camera when the bullet is destroyed
+        try {
+            Camera.main.GetComponent<CameraShake>().Shake();
+        } catch { };
     }
 
     private void Start()
     {
+        // Destroy the object after a specified period of time (lifetime)
         Destroy(gameObject, lifetime);
+
+        // Set runtime reference to audio player component
         audioSource = GetComponent<AudioSource>();
+
+        // Set the trail renderer duration
         GetComponent<TrailRenderer>().time = trailDuration;
     }
 
     private void Update()
     {
-        float velocity = speed * Time.deltaTime;
-        CheckForCollisions(velocity);
-        transform.Translate(Vector3.forward * velocity);
+        // Calculate displacement using speed = distance/time
+        float displacement = speed * Time.deltaTime;
+
+        // Check for collisions against world
+        CheckForCollisions(displacement);
+
+        // Move bullet
+        transform.Translate(Vector3.forward * displacement);
     }
 
     private void CheckForCollisions(float velocity)
     {
+        // Cast ray out
         Ray ray = new Ray(gameObject.transform.position, gameObject.transform.forward);
         RaycastHit hit;
+
+        // Draw debug ray in Unity Editor
         Debug.DrawRay(ray.origin, ray.direction, Color.red);
+        
+        // If any objects are in the way, perform HitObject() function them
         if (Physics.Raycast(ray, out hit, velocity + .1f, collidable)) {
             HitObject(hit, hit.point);
         }
@@ -51,27 +73,42 @@ public class Bullet : MonoBehaviour
 
     private void HitObject(RaycastHit hit, Vector3 hitPoint)
     {
+        // Check if the object can be bounced on
         if (hit.collider.gameObject.layer == bounceable) {
 
+            // If so, play the bounce audio
             audioSource.PlayOneShot(audioSource.clip);
 
             if (bounces >= maxBounces) {
+                // Destroy if Bullet bounced more than max. times
                 Destroy(gameObject);
             } else {
+                // Else increase bounce counter
                 bounces++;
+
+                // Reflect bullet off the wall, using the collider's normal
                 gameObject.transform.forward = Vector3.Reflect((hit.point - gameObject.transform.position), hit.normal);
             }
 
         } else {
             if (hit.collider.gameObject.layer == Globals.PlayerLayer) {
+                // Deal damage to Player if Bullet hit Player 
                 hit.collider.gameObject.GetComponentInParent<Player>().TakeHit(gunDamageAmount, hit.point, transform.forward);
             }
             else if (hit.collider.gameObject.layer == Globals.EnemyLayer) {
+                // Deal damage to Enemy if Bullet hit Enemy
                 hit.collider.gameObject.GetComponentInParent<Enemy>().TakeHit(gunDamageAmount, hit.point, transform.forward);
-                try { FindObjectOfType<GameManager>().enemiesKilled++; } catch { };
+
+                // Increment enemiesKilled counter
+                try {
+                    FindObjectOfType<GameManager>().enemiesKilled++;
+                } catch { };
             } else if(hit.collider.gameObject.layer == Globals.BulletLayer) {
+                // If Bullet hit another Bullet, destroy both Bullets
                 Destroy(hit.collider.gameObject);
             }
+
+            // Else Destroy
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,116 +1,97 @@
-﻿using System.Collections;
+﻿// Dependencies
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
+// Requires a NavMeshAgent component to navigate the terrain/arena
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : Entity
 {
-    public bool shouldAim = true;
-    private Player player;
-    private NavMeshAgent navAgent;
-    private GunController gunController;
-    private Quaternion targetRotation;
+    // Miscellaneous properties
+    public bool shouldAim = true; // Does the Enemy need to aim at the Player 
+    private Player player; // Reference to Player i.e. what it needs to attack and chase
+    private NavMeshAgent navAgent; // Ref to Agent
+    private GunController gunController; // Ref to Gun Controller
     private float halfHeight;
 
-    public enum CurrentTask { Idle, Hunting, Aiming, Shooting, BeDead }
-    private CurrentTask currentTask;
-
     [Header("Movement")]
-    public float moveSpeed;
-    public float turningSpeed;
+    public float moveSpeed; // Move speed
+    public float turningSpeed; // Rotation speed
 
     [Header("AI")]
     [Range(1, 10)]
-    public int intelligence;
+    public int intelligence; // Intelligence 
 
     [Header("Other Attributes")]
-    public LayerMask collideMask;
-    public float maxWalkDistance = 0;
-    public float randomWalkTime = 1f;
-
-    private float randomWalkTimer = 0f;
-    private Vector3 finalPos;
+    public LayerMask collideMask; // What objects can "block its view of the Player"
 
     // Start is called before the first frame update
     protected override void Start()
     {
+        // Set up inherited properties
         base.Start();
+        
+        // Set up variables and initialise runtime references
         player = FindObjectOfType<Player>();
         navAgent = GetComponent<NavMeshAgent>();
+
         if (moveSpeed == 0)
             navAgent.enabled = false;
         else {
             navAgent.speed = moveSpeed;
             navAgent.angularSpeed = turningSpeed;
         }
+
+
         gunController = GetComponentInChildren<GunController>();
-        OnDeath += OnEnemyDeath;
-        currentTask = CurrentTask.Idle;
+
+        // Calculate half the enemy tank's height (used later)
         halfHeight = GetComponent<BoxCollider>().size.y / 2;
-        finalPos = transform.position;
-
     }
 
-    IEnumerator Intelligence1()
-    {
-        gunController.Shoot();
-        yield return new WaitForSeconds(0.2f);
-    }
-
-    IEnumerator Intelligence2()
-    {
-        gunController.Aim(Globals.PlayerNextPosition);
-        gunController.Shoot();
-        yield return new WaitForSeconds(0.2f);
-    }
-
-    IEnumerator Intelligence3()
-    {
-        Vector3 randomDirection = Random.insideUnitSphere * maxWalkDistance;
-        Vector3 nextPos = transform.position + randomDirection;
-        NavMeshHit navMeshHitRef;
-        NavMesh.SamplePosition(randomDirection, out navMeshHitRef, maxWalkDistance, 1);
-        finalPos = navMeshHitRef.position;
-        navAgent.SetDestination(finalPos);
-        yield return new WaitForSeconds(0.2f);
-    }
-
+    // Update contains the Enemy AI Algorithm
     private void Update()
     {
+        // Run artificial intelligence code only if the Player is still alive
         if (player != null)
         {
+            // Aim at the Player
             if(shouldAim)
                 gunController.Aim(player.transform.position);
+
+            // Check for objects blocking its view of the Player
             Ray ray = new Ray(transform.position, new Vector3(player.transform.position.x, transform.position.y + halfHeight, player.transform.position.z) - new Vector3(transform.position.x, transform.position.y + halfHeight, transform.position.z));
             RaycastHit hit;
 
+            // Is there a direct line of sight to the Player?
             bool directLineofSight = !Physics.Raycast(ray, out hit, Vector3.Distance(player.transform.position, transform.position), collideMask);
 
             Debug.DrawRay(ray.origin, ray.direction * Vector3.Distance(player.transform.position, transform.position), directLineofSight ? Color.green : Color.red);
+
+            // If the Enemy can "see" the Player
             if (directLineofSight)
             {
                 switch (intelligence)
                 {
                     case 2:
+                        // Aim at the player
                         gunController.Aim(Globals.PlayerNextPosition);
                         break;
                     default: break;
                 }
+
+                // Shoot at the Player
                 gunController.Shoot();
             }
             else
             {
+                // If the Player is obscured from vision, move till it is in view (and therefore 'shootable')
                 if (navAgent.isActiveAndEnabled && (player != null))
                     navAgent.SetDestination(player.transform.position);
             }
         }
-    }
-
-    void OnEnemyDeath()
-    {
-        currentTask = CurrentTask.BeDead;
     }
 
 }
