b8b8957e6762f261b61eb6263b538c7858db3e85
Ruchir Bapat <ruchir.bapat@gmail.com>
2019-06-05 02:21:29 +1000

    Finished game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -150,7 +150,6 @@ public class GameManager : MonoBehaviour
     public void NextLevel() {
         backToMenu = false;
         StartCoroutine(LoadSceneCoroutine(true));
-        print("done couroutine!!");
     }
 
     IEnumerator LoadSceneCoroutine(bool nextLevel)
